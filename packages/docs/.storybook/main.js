6a0e3408c37bf571bef4de2253200b2f62d03862
Fernando Andrade <fernandofreamunde@gmail.com>
2023-05-08 21:24:03 +0200

    :wrench: set some configs for storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,10 +1,15 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.mdx", 
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-docs', 
+    '@storybook/preset-typescript',
   ],
   framework: {
     name: "@storybook/react-webpack5",
